09c38f4d7de9201fae2e08d792ee7a034d0fd0ec
John Dakin <jdakin749@gmail.com>
2024-10-30 12:09:21 +0300

    Add files via upload

---
+++ Polymorphism/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Shapes shape  = new Shapes();
+		Circle circle = new Circle();
+		Shapes square = new Square();
+		
+		circle.area();
+
+	}
+
+}
